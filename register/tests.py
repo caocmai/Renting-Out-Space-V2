bbecfd0170a26ae1aab5b194da484cf4023c90bf
Cao Mai <caomai@rocketmail.com>
2019-12-13 12:08:29 -0800

    [add] testings

--- register/tests.py
+++ register/tests.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from register.forms import UserRegistrationForm
 
+class RegisterViewTest(TestCase):
+        
+        
+    def test_one_page(self):
+        response = self.client.get(reverse_lazy('sign-up-page'))
+        self.assertEqual(response.status_code, 200)
+
+
 class FormCreationTest(TestCase):
     """ To test form creation """
 
